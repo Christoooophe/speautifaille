e066b9b91052d7b16da6031dacc56eb7ae1c7d6c
Christoooophe <christophealfier02@gmail.com>
2023-02-07 18:54:27 +0100

    ajout pages

---
+++ pages/PrincipalePage.jsx
@@ -0,0 +1,11 @@
+// Cette page va recevoir tous les éléments du site genre la barre de nav, la barre du son en cours ...
+import React from 'react';
+import { View } from 'react-native';
+
+export default function PrincipalePage(props) {
+    return (
+        <View>
+            
+        </View>
+    );
+}
