18ea633847d156018c3035649fa208d21cb5df92
Christoooophe <christophealfier02@gmail.com>
2023-02-11 00:24:00 +0100

    Ajout scrollview, design tab, page search...

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,11 +1,24 @@
 // Cette page va recevoir tous les éléments du site genre la barre de nav, la barre du son en cours ...
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 
 export default function HomePage(props) {
     return (
-        <View>
-            <Text>yo</Text>
+        <View style={styles.container}>
+            <ScrollView>
+                <Text style={styles.text}>yo</Text>
+            </ScrollView>
+            
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor: '#000000',
+        flex : 1
+    },
+    text : {
+        color : 'white'
+    }
+  });
--- pages/SearchPage.jsx
+++ pages/SearchPage.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, TextInput } from 'react-native';
+import Parcourir from '../components/parcourir/Parcourir';
 
 const SearchPage = () => {
     return (
-        <View>
-            <Text>efefeqfefeq</Text>
+        <View style={styles.container}> 
+            <Text style={styles.text}>Rechercher</Text>
+            <TextInput style={styles.input} placeholder='Recherchez votre artiste préféré'/>
+            <ScrollView>
+                <View style={styles.parcourirContainer}>
+                    <Parcourir image={require('../assets/electro.jpg')} titre='electro'/>
+                    <Parcourir image={require('../assets/jazz.png')} titre='jazz'/>
+                    <Parcourir image={require('../assets/rap.png')} titre='rap'/>
+                    <Parcourir image={require('../assets/techno.jpg')} titre='techno'/>
+                    <Parcourir image={require('../assets/electro.jpg')} titre='electro'/>
+                    <Parcourir image={require('../assets/jazz.png')} titre='jazz'/>
+                    <Parcourir image={require('../assets/rap.png')} titre='rap'/>
+                    <Parcourir image={require('../assets/techno.jpg')} titre='techno'/>
+                </View>
+            </ScrollView>
         </View>
     );
 };
 
 export default SearchPage;
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor: '#000000',
+        flex : 1,
+        paddingTop : 50
+    },
+    text : {
+        color : 'white',
+        fontSize : 25,
+        marginBottom : 10,
+        paddingLeft : 10
+    },
+    parcourirContainer : {
+        display : 'flex',
+        flexDirection : 'row',
+        flexWrap : 'wrap',
+        alignItems : 'center',
+        justifyContent : 'space-around',
+    },
+    input : {
+        width : 300,
+        height : 50,
+        backgroundColor : 'white',
+        borderRadius : 30,
+        margin : 10,
+        marginTop : 0,
+        fontSize : 15,
+        paddingLeft : 10,
+        marginBottom : 30
+    }
+  });
